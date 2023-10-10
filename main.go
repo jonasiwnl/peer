dfbf7b03d2c6e7171b511b05396732146083c95c
arael <jonasg@umich.edu>
2023-10-09 23:30:04 -0400

    refactor peer & start fout

--- main.go
+++ main.go
@@ -2,43 +2,51 @@ package main
 
 import (
 	"log"
+	"os"
 
 	ui "github.com/gizak/termui/v3"
 	"github.com/gizak/termui/v3/widgets"
 )
 
-func save(selected string) {
-	// Write to text file
+type Peer struct {
+	output   *os.File
+	paused   bool
+	selected string
 }
 
-func main() {
+func (p *Peer) save() {
+	_, err := p.output.WriteString(p.selected + "\n")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (p *Peer) run() {
 	if err := ui.Init(); err != nil {
 		log.Fatalf("failed to initialize termui: %v", err)
 	}
 	defer ui.Close()
 
-	p := widgets.NewParagraph()
-	p.Text = "PEER"
-	p.SetRect(0, 0, 25, 5)
+	para := widgets.NewParagraph()
+	para.Text = "PEER"
+	para.SetRect(0, 0, 25, 5)
 
 	controls := widgets.NewParagraph()
 	controls.Text = "|q| Quit \t |p| Pause \t |s| Save"
 	controls.SetRect(0, 6, 33, 7)
 
-	ui.Render(p, controls)
-
-	// State
-	paused := false
-	selected := ""
+	ui.Render(para, controls)
 
 	for e := range ui.PollEvents() {
 		switch e.ID {
 		case "q", "<C-c>": // press 'q' or 'C-c' to quit
 			return
 		case "p":
-			paused = !paused
+			p.paused = !p.paused
 		case "s":
-			save(selected)
+			p.save()
+
 		// Maybe?
 		case "<Resize>":
 			// payload := e.Payload.(ui.Resize)
@@ -46,4 +54,23 @@ func main() {
 		}
 		// We need another event for mouse clicks, switch selected
 	}
+}
+
+func main() {
+	output, err := os.Create("output.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// close output file on exit and check for its returned error
+	defer func() {
+		if err := output.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	p := Peer{output, false, ""}
+	p.run()
+
+	log.Println("Exiting...")
 }
